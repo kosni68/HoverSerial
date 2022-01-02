c495d5d76af34748c4ab8138ecbcc8cf8366b624
kosni68 <nicolas68250@gmail.com>
2022-01-02 13:15:59 +0100

    update

--- HoverSerial/HoverSerial.py
+++ HoverSerial/HoverSerial.py
@@ -1,4 +1,3 @@
-import struct # for values to bytes
 import serial # to communicate with the hoverboard
 
 class Hoverboard_serial:
@@ -6,19 +5,24 @@ class Hoverboard_serial:
     def __init__(self,adresse,baud):
         self.uart = serial.Serial(adresse, baud, timeout=1)
         self.startBytes = bytes.fromhex('ABCD')[::-1] # lower byte first
-        self.incomingBytesPrev = bytes.fromhex('00')
+        self.incomingBytesPrev = bytes()
         
     
     def send_command(self,steer, speed):
         '''
-        Creates a bytearray for controlling the hoverboard
+        Send a bytearray for controlling the hoverboard
 
         :param steer: -1000...1000
         :param speed: -1000...1000
-        :returns: command bytes
+
+        bytearray :
+        Start frame(unsigned int16) : 0xABCD
+        Steer(signed int16) : Steer with range -1000 to 1000
+        Speed(signed int16) : Speed with range -1000 to 1000
+        Checksum(unsigned int16) : XOR checksum
         '''
-        steerBytes = struct.pack('h', steer)
-        speedBytes = struct.pack('h', speed)
+        steerBytes = steer.to_bytes(2, byteorder="little")
+        speedBytes = speed.to_bytes(2, byteorder="little")
         checksumBytes = bytes(a^b^c for (a, b, c) in zip(self.startBytes, steerBytes, speedBytes))
 
         command = self.startBytes+steerBytes+speedBytes+checksumBytes
@@ -26,29 +30,43 @@ class Hoverboard_serial:
         self.uart.write(command)
         
     def receive_feedback(self):
-        
+        '''
+        receive a bytearray to the hoverboard 
+        return: bytearray convert to dictionnary
+
+        bytearray :
+        Start frame(unsigned int16) : 0xABCD
+        Cmd1(signed int16) : Steer or Brake(hovercar) after normalizing and mixing
+        Cmd2(signed int16) : Speed or Throttle(hovercar) after normalizing and mixing
+        SpeedR(signed int16) : Measured right wheel speed in RPM
+        SpeedL(signed int16) : Measured left wheel speed in RPM
+        Battery Voltage(signed int16) : Calibrated Battery Voltage *100
+        Temperature(signed int16) : Temperature in °C *10
+        Led(unsigned int16) : Used to control the leds on the sideboard
+        Checksum(unsigned int16) : XOR checksum
+
+        '''
+
         incomingByte = self.uart.read()
-        #bufStartFrame = struct.pack('cc',self.incomingBytesPrev,incomingByte)
         bufStartFrame = self.incomingBytesPrev+incomingByte
-        #print("bufStartFrame: ",bufStartFrame)
         
         if bufStartFrame != self.startBytes:
             self.incomingBytesPrev=incomingByte
             return
         else:
-            feedback_dict = {"cmd1":0, "cmd2":0, "speedR_meas":0, "speedL_meas":0, "batVoltage":0, "boardTemp":0, "cmdLed":0}
+            feedback = {"cmd1":0, "cmd2":0, "speedR_meas":0, "speedL_meas":0, "batVoltage":0, "boardTemp":0, "cmdLed":0}
             checksumBytes=bufStartFrame
-            for key,value in feedback_dict.items():
-                bytes_element=self.uart.read(2)
-                feedback_dict[key] = struct.unpack('h', bytes_element)
-                #print(key,bytes_element)
-                #print(struct.unpack('h', bytes_element))
-                checksumBytes = bytes(a^b for (a, b) in zip(checksumBytes, bytes_element))
-                
+    
+            for element in feedback.items():
+                elementBytes=self.uart.read(2)
+                feedback[element]=int.from_bytes(elementBytes, byteorder='big')
+                checksumBytes = bytes(a^b for (a, b) in zip(checksumBytes, elementBytes))
+
+            #print(feedback,feedback)
             lastBytes=self.uart.read(2)
             if checksumBytes == lastBytes:
                 print("checksumBytes True")
-                return feedback_dict
+                return feedback
             
             return
             
--- HoverSerial/main.py
+++ HoverSerial/main.py
@@ -1,41 +1,76 @@
 from HoverSerial import*
+import threading
 import time
 
-def main():
-    
-    SERIAL_PORT = '/dev/serial0'
-    SERIAL_BAUD = 38400
 
-    hover_serial=Hoverboard_serial(SERIAL_PORT,SERIAL_BAUD)
+def thread_send_command():
 
-    SPEED_MAX_TEST = 300
-    iTestMax = SPEED_MAX_TEST
+    #constantes
+    SPEED_MAX_TEST = 300  # [-] Maximum speed for testing
+    SPEED_STEP = 20  # [-] Speed step
+    TIME_SEND = 100  # [ms] Sending time interval
+
+    #local variables
+    iStep = SPEED_STEP
     iTest = 0
     steer = 0
-    
+    start_time = 0
+
     while True:
-        
+
+        # Calculate elapsed_time
+        elapsed_time = time.time() - start_time
+        if elapsed_time < TIME_SEND:
+            continue
+        start_time = time.time()
+
+        # Calculate test command speed
         speed = SPEED_MAX_TEST-2*abs(iTest)
-        command = hover_serial.send_command(steer, 100)
-        #print(f'\nSending:\nspeed: {speed}, command: {command}')
-        
-        #print('Receiving:')
+
+        # Send commands
+        hover_serial.send_command(steer, 100)
+        print('Sending:\t steer: '+str(steer)+'speed: '+str(speed))
+
+        # invert step if reaching limit
+        iTest += iStep
+        if (iTest >= SPEED_MAX_TEST or iTest <= -SPEED_MAX_TEST):
+            iStep = -iStep
+
+
+def thread_receive_feedback():
+
+    while True:
+
         feedback = hover_serial.receive_feedback()
-        #feedback = hover_serial.read_all()
-        print(feedback)
-                
-        # calculate next test speed
-        iTest += 10
-        if (iTest > iTestMax):
-            iTest = -iTestMax
-        
-        time.sleep(0.001)
-        
+
+        if feedback == None:
+            continue
+        else:
+            print('Receiving:\t', feedback)
+
 
 if __name__ == "__main__":
-    
+
+    SERIAL_PORT = '/dev/serial0'
+    SERIAL_BAUD = 38400
+    hover_serial = Hoverboard_serial(SERIAL_PORT, SERIAL_BAUD)
+
     try:
-        main()
+
+        thread1 = threading.Thread(target=thread_send_command)
+        thread1.start()
+
+        thread2 = threading.Thread(target=thread_receive_feedback)
+        thread2.start()
+
+        thread1.join()
+        thread2.join()
+
     except KeyboardInterrupt:
+        print("Keyboard interrupt...")
+
+    except Exception as e:
+        print("Error: " + str(e))
+
+    finally:
         hover_serial.close()
-            
